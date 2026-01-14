68f8e19543e0d4c570dc39dbb141f5db1b652409
AdityaNMGHUB <mishraadityanarayan4@gmail.com>
2026-01-14 14:57:42 +0530

    Delete newreverse.js.js

--- newreverse.js.js
+++
@@ -1,6 +0,0 @@
-function reverseString(str) {
-    return str.split(" ").reverse().join(" ");
-}
-let String = "Java is Fun";
-console.log("Original String: " + String);
-console.log("reversed string: "+ reverseString(String));
