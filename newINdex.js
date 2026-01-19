050105acf5c6cf840fed8831566e4b6e10019ba1
AdityaNMGHUB <mishraadityanarayan4@gmail.com>
2026-01-19 10:47:38 +0530

    2nd new index file

---
+++ newINdex.js
@@ -0,0 +1,10 @@
+String = "hello";
+console.log(String.length);
+console.log(String.toUpperCase());
+console.log(String.toLowerCase());
+console.log(String.split("*"));
+console.log(String.replace("*","$"));
+console.log(String.indexOf("e"));
+console.log(String.startsWith("h"));
+console.log(String.replaceAll("llo","llllloooo"))
+console.log(String.charAt(2));
